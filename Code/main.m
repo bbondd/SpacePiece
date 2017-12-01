8e5adbb10cc59bd4be25ef1963022968c04e682c
DESKTOP-D2J5D7R\jo_ch <jo_chestnut@naver.com>
2017-12-01 18:29:25 +0900

    addParfor

--- Code/main.m
+++ Code/main.m
@@ -188,9 +188,9 @@ limitPlanesA = getLimitPlanesFromImage(viewVectors(:,1), imageA, rotateAngles(1)
 limitPlanesB = getLimitPlanesFromImage(viewVectors(:,2), imageB, rotateAngles(2));
 limitPlanesC = getLimitPlanesFromImage(viewVectors(:,3), imageC, rotateAngles(3));
 
-xyzPoints = zeros(size(limitPlanesA, 1) * size(limitPlanesB, 1) * size(limitPlanesC, 1), 3);
-count = 1;
-for i = 1:1:size(limitPlanesA, 1)
+A(size(limitPlanesA, 1)).B(size(limitPlanesB, 1)).C(size(limitPlanesC, 1)).xyzPoint = [];
+
+parfor i = 1:1:size(limitPlanesA, 1)
     for j = 1:1:size(limitPlanesB, 1)
         for k = 1:1:size(limitPlanesC, 1)
             tempXYZpoint = [limitPlanesA(i).plane; limitPlanesB(j).plane; limitPlanesC(k).plane];
@@ -199,12 +199,31 @@ for i = 1:1:size(limitPlanesA, 1)
             boolC = isXYZpointInLimitPlane(limitPlanesC(k), tempXYZpoint);
             
             if boolA && boolB && boolC
-                xyzPoints(count, :) = transpose(matrixToCoordinate(tempXYZpoint));
+                A(i).B(j).C(k).xyzPoint = transpose(matrixToCoordinate(tempXYZpoint));
+            else
+                A(i).B(j).C(k).xyzPoint = [];
+            end
+        end
+    end
+end
+
+
+xyzPoints = zeros(size(limitPlanesA, 1) * size(limitPlanesB, 1) * size(limitPlanesC, 1), 3);
+
+count = 1;
+for i = 1:1:size(limitPlanesA, 1)
+    for j = 1:1:size(limitPlanesB, 1)
+        for k = 1:1:size(limitPlanesC, 1)
+            if size(A(i).B(j).C(k).xyzPoint, 2) ~= 0
+                xyzPoints(count,:) = A(i).B(j).C(k).xyzPoint;
                 count = count + 1;
             end
         end
     end
 end
+
+
+
 
 xyzPoints([count:1:size(xyzPoints, 1)], :) = [];
 end
